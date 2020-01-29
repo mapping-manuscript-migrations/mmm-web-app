c4e6d3d619896c413290ce0a2080a2f60e59d034
esikkala <esko.ikkala@aalto.fi>
2020-01-29 07:55:48 +0200

    Update footer layout, add esrc logo

--- src/client/components/main_layout/Footer.js
+++ src/client/components/main_layout/Footer.js
@@ -12,6 +12,7 @@ import aaltoLogo from '../../img/logos/partners/Aalto_SCI_EN_13_BLACK_2_cropped.
 // import secoLogo from '../../img/logos/partners/13_9_04_logo_cropped_small.png'
 import tapLogo from '../../img/logos/funders/cropped-logo_tap_0_.png'
 import didLogo from '../../img/logos/funders/did_logo.png'
+import esrcLogo from '../../img/logos/funders/UKRI_ESR_Council-Logo_Horiz-RGB_100px.png'
 import imlsLogo from '../../img/logos/funders/imls_logo_2c_cropped.jpg'
 import anrLogo from '../../img/logos/funders/Agence_Nationale_de_la_Recherche.png'
 import akaLogo from '../../img/logos/funders/aka_en_vaaka_rgb.jpg'
@@ -30,9 +31,9 @@ const styles = theme => ({
   },
   layout: {
     width: 'auto',
-    height: 120,
+    // height: 120,
     marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
+    marginBottom: theme.spacing(1.5),
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
     [theme.breakpoints.up(1500 + theme.spacing(6))]: {
@@ -55,6 +56,13 @@ const styles = theme => ({
   bodleainLogo: {
     height: 48,
     marginTop: -5
+  },
+  tapLogo: {
+    height: 40,
+    marginRight: -30
+  },
+  esrcLogo: {
+    height: 40
   }
 })
 
@@ -63,64 +71,67 @@ const Footer = props => {
   return (
     <Paper className={classes.root}>
       <Grid container className={classes.layout}>
-        <Grid container spacing={3} item xs={12}>
-          <Grid item xs className={classes.logoContainer}>
-            <a href='http://www.oerc.ox.ac.uk' target='_blank' rel='noopener noreferrer'>
-              <img className={classes.logo} src={oxfordLogo2} alt='logo' />
-            </a>
-          </Grid>
-          <Grid item xs className={classes.logoContainer}>
-            <a href='https://www.bodleian.ox.ac.uk/' target='_blank' rel='noopener noreferrer'>
-              <img className={classes.bodleainLogo} src={bodleainLogo} alt='logo' />
-            </a>
-          </Grid>
-          <Grid item xs className={classes.logoContainer}>
-            <a href='https://schoenberginstitute.org' target='_blank' rel='noopener noreferrer'>
-              <img className={classes.logo} src={pennLogo} alt='logo' />
-            </a>
-          </Grid>
-          <Grid item xs className={classes.logoContainer}>
-            <a href='https://www.irht.cnrs.fr/?q=en' target='_blank' rel='noopener noreferrer'>
-              <img className={classes.logo} src={cnrsLogo} alt='logo' />
-            </a>
-          </Grid>
-          {/* <Grid item xs className={classes.logoContainer}>
+        <Grid item xs className={classes.logoContainer}>
+          <a href='http://www.oerc.ox.ac.uk' target='_blank' rel='noopener noreferrer'>
+            <img className={classes.logo} src={oxfordLogo2} alt='logo' />
+          </a>
+        </Grid>
+        <Grid item xs className={classes.logoContainer}>
+          <a href='https://www.bodleian.ox.ac.uk' target='_blank' rel='noopener noreferrer'>
+            <img className={classes.bodleainLogo} src={bodleainLogo} alt='logo' />
+          </a>
+        </Grid>
+        <Grid item xs className={classes.logoContainer}>
+          <a href='https://schoenberginstitute.org' target='_blank' rel='noopener noreferrer'>
+            <img className={classes.logo} src={pennLogo} alt='logo' />
+          </a>
+        </Grid>
+        <Grid item xs className={classes.logoContainer}>
+          <a href='https://www.irht.cnrs.fr/?q=en' target='_blank' rel='noopener noreferrer'>
+            <img className={classes.logo} src={cnrsLogo} alt='logo' />
+          </a>
+        </Grid>
+        {/* <Grid item xs className={classes.logoContainer}>
             <a href='https://seco.cs.aalto.fi/projects/mmm' target='_blank' rel='noopener noreferrer'>
               <img className={classes.logo} src={secoLogo} alt='logo' />
             </a>
           </Grid> */}
-          <Grid item xs className={classes.logoContainer}>
-            <a href='https://www.aalto.fi/en/department-of-computer-science' target='_blank' rel='noopener noreferrer'>
-              <img className={classes.aaltoLogo} src={aaltoLogo} alt='logo' />
-            </a>
-          </Grid>
+        <Grid item xs className={classes.logoContainer}>
+          <a href='https://www.aalto.fi/en/department-of-computer-science' target='_blank' rel='noopener noreferrer'>
+            <img className={classes.aaltoLogo} src={aaltoLogo} alt='logo' />
+          </a>
         </Grid>
-        <Grid container spacing={3} item xs={12}>
-          <Grid item xs className={classes.logoContainer}>
-            <a href='https://www.transatlanticplatform.com' target='_blank' rel='noopener noreferrer'>
-              <img className={classes.logo} src={tapLogo} alt='logo' />
-            </a>
-          </Grid>
-          <Grid item xs className={classes.logoContainer}>
-            <a href='https://diggingintodata.org/awards/2016/project/mapping-manuscript-migrations-digging-data-history-and-provenance-pre-modern' target='_blank' rel='noopener noreferrer'>
-              <img className={classes.logo} src={didLogo} alt='logo' />
-            </a>
-          </Grid>
-          <Grid item xs className={classes.logoContainer}>
-            <a href='https://www.imls.gov' target='_blank' rel='noopener noreferrer'>
-              <img className={classes.logo} src={imlsLogo} alt='logo' />
-            </a>
-          </Grid>
-          <Grid item xs className={classes.logoContainer}>
-            <a href='https://anr.fr/en' target='_blank' rel='noopener noreferrer'>
-              <img className={classes.logo} src={anrLogo} alt='logo' />
-            </a>
-          </Grid>
-          <Grid item xs className={classes.logoContainer}>
-            <a href='https://www.aka.fi/en' target='_blank' rel='noopener noreferrer'>
-              <img className={classes.logo} src={akaLogo} alt='logo' />
-            </a>
-          </Grid>
+      </Grid>
+      <Grid container className={classes.layout}>
+        <Grid item xs className={classes.logoContainer}>
+          <a href='https://www.transatlanticplatform.com' target='_blank' rel='noopener noreferrer'>
+            <img className={classes.tapLogo} src={tapLogo} alt='logo' />
+          </a>
+        </Grid>
+        <Grid item xs className={classes.logoContainer}>
+          <a href='https://diggingintodata.org/awards/2016/project/mapping-manuscript-migrations-digging-data-history-and-provenance-pre-modern' target='_blank' rel='noopener noreferrer'>
+            <img className={classes.logo} src={didLogo} alt='logo' />
+          </a>
+        </Grid>
+        <Grid item xs className={classes.logoContainer}>
+          <a href='https://esrc.ukri.org' target='_blank' rel='noopener noreferrer'>
+            <img className={classes.esrcLogo} src={esrcLogo} alt='logo' />
+          </a>
+        </Grid>
+        <Grid item xs className={classes.logoContainer}>
+          <a href='https://www.imls.gov' target='_blank' rel='noopener noreferrer'>
+            <img className={classes.logo} src={imlsLogo} alt='logo' />
+          </a>
+        </Grid>
+        <Grid item xs className={classes.logoContainer}>
+          <a href='https://anr.fr/en' target='_blank' rel='noopener noreferrer'>
+            <img className={classes.logo} src={anrLogo} alt='logo' />
+          </a>
+        </Grid>
+        <Grid item xs className={classes.logoContainer}>
+          <a href='https://www.aka.fi/en' target='_blank' rel='noopener noreferrer'>
+            <img className={classes.logo} src={akaLogo} alt='logo' />
+          </a>
         </Grid>
       </Grid>
     </Paper>
